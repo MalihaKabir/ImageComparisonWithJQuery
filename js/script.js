dd828e3120784c0bfa77ddbcb9d878229c35a68c
MalihaKabir <malihakabir01@gmail.com>
2019-03-25 10:40:59 +0600

    adding all files and folders

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// $(function() {
+//   $(".box[data-orientation!='vertical']").twentytwenty({default_offset_pct:0.5});
+// });
+//
+// // // OR
+// //
+// // $(function() {
+// //   $(".box").twentytwenty();
+// // });
+
+
+// To understand "{default_offset_pct:0.3}":
+$(function() {
+  $("#img1[data-orientation!='vertical']").twentytwenty({default_offset_pct:0.3});
+});
+
+$(function() {
+  $("#img2[data-orientation!='vertical']").twentytwenty({default_offset_pct:0.5});
+});
+
+$(function() {
+  $("#img3[data-orientation!='vertical']").twentytwenty({default_offset_pct:0.8});
+});
+
+$(function() {
+  $("#img4[data-orientation!='vertical']").twentytwenty({default_offset_pct:0.5});
+});
+
+$(function() {
+  $("#img5[data-orientation!='vertical']").twentytwenty({default_offset_pct:0.5});
+});
